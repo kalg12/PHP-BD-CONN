2f19d26fab5ba36a5540174f6703624fd1cbf088
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-21 10:14:18 -0500

    Incorporación de Navbar en el index

--- index.php
+++ index.php
@@ -9,6 +9,20 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+    <!-- Etiqueta para insertar código en PHP -->
+    <?php
+
+    require_once "./views/navbar.php";
+
+    ?>
+
+
+
+
+
+
+
+
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
