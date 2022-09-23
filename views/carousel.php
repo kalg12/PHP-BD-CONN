41091cd3b3ad67e0d5a0256dd12b4e737916c38e
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-23 00:22:46 -0500

    Image resolution and quality resolved

--- views/carousel.php
+++ views/carousel.php
@@ -4,10 +4,10 @@
       <img src="./src/img/toystory4.png" class="d-block w-100" alt="Toy Story 4">
     </div>
     <div class="carousel-item">
-      <img src="./src/img/st4.jpeg" class="d-block w-100" alt="Stranger Things 4">
+      <img src="./src/img/st4.jpg" class="d-block w-100" alt="Stranger Things 4">
     </div>
     <div class="carousel-item">
-      <img src="./src/img/moonknight.jpeg" class="d-block w-100" alt="Moon Knight">
+      <img src="./src/img/moonknight.jpg" class="d-block w-100" alt="Moon Knight">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
