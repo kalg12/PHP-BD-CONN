5cf68302d10ced8662c5b46020aef1d5fa3fcebc
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-28 10:15:26 -0500

    Añadiendo un formulario

--- index.php
+++ index.php
@@ -12,19 +12,10 @@
 <body>
     <!-- Etiqueta para insertar código en PHP -->
     <?php
-
     require_once "./views/navbar.php";
     require_once "./views/carousel.php";
-
+    require_once "./views/formulario.php";
     ?>
-
-
-
-
-
-
-
-
     <!-- JavaScript Bundle with Popper -->
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 </body>
---
+++ views/formulario.php
@@ -0,0 +1,17 @@
+<div class="container mt-5 mb-5">
+<form action="index.php" method="post">
+    <div class="form-group mt-3">
+        <label for="nombre">Nombre</label>
+        <input type="text" class="form-control" id="nombre" name="nombre" placeholder="Nombre de la película">
+    </div>
+    <div class="form-group mt-3">
+        <label for="categoria">Categoría</label>
+        <input type="text" class="form-control" id="categoria" name="categoria" placeholder="Categoría de la película">
+    </div>
+    <div class="form-group mt-3">
+        <label for="duracion">Duración</label>
+        <input type="number" class="form-control" id="duracion" name="duracion" placeholder="Duración de la película">
+    </div>
+    <button type="submit" class="btn btn-primary mt-3">Guardar</button>
+</form>
+</div>
