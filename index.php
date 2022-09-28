98895ef6327551d1b960b7b8cb4074df4a79c4c5
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-09-28 09:35:02 -0500

    Vinculando hoja de estilos al archivo principal

--- index.php
+++ index.php
@@ -7,6 +7,7 @@
     <title>Películas</title>
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+<link rel="stylesheet" href="./src/styles/styles.css">
 </head>
 <body>
     <!-- Etiqueta para insertar código en PHP -->
