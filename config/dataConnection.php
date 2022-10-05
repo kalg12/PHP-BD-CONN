352eb1030389587d6d68769939e3516190b72644
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-05 09:33:04 -0500

    Usando diferente conexión

--- config/dataConnection.php
+++ config/dataConnection.php
@@ -5,7 +5,8 @@ class Conexion
     public function conectar()
     {
         /* Necesitamos 4 parametros: servidor, usuario, contraseña, nombre_bd */
-        $enlace = mysqli_connect('localhost', 'root', '', 'peliculas');
+        /* localhost = host; root = user; '' = password; 'peliculas' = database */
+        $enlace = mysqli_connect('sql521.main-hosting.eu', 'u413489667_equipoTres', '@equipoTres202240', 'u413489667_equipoTres');
         if($enlace){
             echo "Conexión correcta a la base de datos, si pude mamá";
         }
