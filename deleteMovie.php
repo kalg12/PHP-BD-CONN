b4c59765799046c309e4d52ffb24cdd02ad3b360
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-26 10:49:43 -0500

    Descripción del código

--- deleteMovie.php
+++ deleteMovie.php
@@ -2,6 +2,7 @@
 require_once './models/Movie.php';
 $movie = new Movie(); //Instanciar la clase
 
+/* Checking if the idPelicula is set, if it is, it will delete the movie. */
 if (isset($_GET['idPelicula'])) {
     $idPelicula = $_GET['idPelicula'];
     $movie->deleteMovie($idPelicula);
