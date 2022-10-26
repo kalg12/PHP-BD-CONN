c010af0a93a285e186fb898537aaa0632ba5dbe2
Kevin Luciano <78965771+kalg12@users.noreply.github.com>
2022-10-26 10:45:15 -0500

    Función eliminar añadida

--- models/Movie.php
+++ models/Movie.php
@@ -25,6 +25,19 @@ class Movie extends Conexion{
         return $result;
     }
 
+    //Para editar y eliminar necesitamos pasar un parámetro para indicar que elemento vamos a borrar
+    public function deleteMovie($idPelicula){
+        $sql = "DELETE FROM peliculas WHERE idPelicula = '$idPelicula'";
+        $result = mysqli_query($this->conectar(), $sql);
+
+        //Código para hacer efecto de actualización en la página
+        if($result){
+            Header("Location: index.php");
+        }else {
+            echo "Hubo un error al intentar eliminar el dato";
+        }
+    }
+
 }
 
 ?>
